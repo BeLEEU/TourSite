00fcead85be7284c1952699c178e37c305b9fb0e
BeLEEU <531357870@qq.com>
2021-08-17 22:38:27 +0800

    ui update / add sidemenu and carousel

--- reactts/src/App.tsx
+++ reactts/src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.module.css';
-import {Header, Footer} from './components'
+import {Header, Footer, SideMenu, Carousel} from './components'
+import {Row, Col} from 'antd'
+import styles from './App.module.css'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className={styles['page-content']}>
+        <Row style={{marginTop: 10}}>
+          <Col span={6}>
+            <SideMenu/>
+          </Col>
+          <Col span={18}>
+            <Carousel/>
+          </Col>
+        </Row>
+      </div>
       <Footer/>
     </div>
   );
