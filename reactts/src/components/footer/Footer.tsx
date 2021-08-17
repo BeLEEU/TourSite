856dd41f87da852a9d99aed554f3447a7db3ca53
BeLEEU <531357870@qq.com>
2021-08-17 21:45:49 +0800

    add header and footer

--- reactts/src/components/footer/Footer.tsx
+++ reactts/src/components/footer/Footer.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Layout, Typography} from "antd";
 
-export const Footer = () => {
+export const Footer: React.FC = () => {
     
-    return(
-        <div>Footer</div>
-    );
+  return(
+    <Layout.Footer >
+      <Typography.Title level={2} style={{textAlign:"center"}}>
+        版权所有 @ LEE旅游网
+      </Typography.Title>
+    </Layout.Footer>
+  );
 }
--- reactts/src/components/header/Header.tsx
+++ reactts/src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from './Header.module.css'
+import logo from '../../assets/logo.svg'
 import { Typography, Menu, Layout, Input, Button, Dropdown} from "antd";
 import { GlobalOutlined } from "@ant-design/icons";
 
@@ -28,6 +29,33 @@ export const Header: React.FC = () => {
           </Button.Group>
         </div>
       </div>
+      <Layout.Header className={styles['main-header']}>
+        <img src={logo} alt='Logo' className={styles['App-logo']}/>
+        <Typography.Title level={3} className={styles['title']}>
+          React版本旅游网
+        </Typography.Title>
+        <Input.Search className={styles['search']}
+        placeholder='请输入目的地、旅游地点'>
+        </Input.Search>
+      </Layout.Header>
+      <Menu mode={'horizontal'} className={styles['main-menu']}>
+        <Menu.Item key={1}>旅游首页</Menu.Item>
+        <Menu.Item key={2}>周末游</Menu.Item>
+        <Menu.Item key={3}>跟团游</Menu.Item>
+        <Menu.Item key="4"> 自由行 </Menu.Item>
+        <Menu.Item key="5"> 私家团 </Menu.Item>
+        <Menu.Item key="6"> 邮轮 </Menu.Item>
+        <Menu.Item key="7"> 酒店+景点 </Menu.Item>
+        <Menu.Item key="8"> 当地玩乐 </Menu.Item>
+        <Menu.Item key="9"> 主题游 </Menu.Item>
+        <Menu.Item key="10"> 定制游 </Menu.Item>
+        <Menu.Item key="11"> 游学 </Menu.Item>
+        <Menu.Item key="12"> 签证 </Menu.Item>
+        <Menu.Item key="13"> 企业游 </Menu.Item>
+        <Menu.Item key="14"> 高端游 </Menu.Item>
+        <Menu.Item key="15"> 爱玩户外 </Menu.Item>
+        <Menu.Item key="16"> 保险 </Menu.Item>         
+      </Menu>
     </div>
   );
 }
